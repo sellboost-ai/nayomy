f20f2451e5375c7c771e32a06e69c682dd83f927
Cem Keklik <cemkeklik@yandex.com>
2026-05-26 18:56:37 +0300

    seo: sitemap + robots.txt (1162 URLs)

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,12 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://nayomy.com',
-  integrations: [mdx()],
+  integrations: [mdx(), sitemap()],
   i18n: {
     defaultLocale: 'en',
     locales: ['en', 'tr'],
